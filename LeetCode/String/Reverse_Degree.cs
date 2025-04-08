b7c41cb8169fa3b8189088bab4e3153f00b71fb7
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-08 08:31:01 +0530

    Reverse_Degree

---
+++ LeetCode/String/Reverse_Degree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.String
+{
+    internal class Reverse_Degree
+    {
+        public int ReverseDegree(string s)
+        {
+            int ans = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var loc = 'z'-s[i]+1;
+                ans += loc * (i + 1);
+            }
+            return ans;
+        }
+    }
+}
+
