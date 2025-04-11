acb2d1ceee5ae8e9ab44131441d04b6175ad2688
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-11 23:23:43 +0530

    11-04-25   #1

---
+++ LeetCode/String/Easy/ArrayStringsAre_Equal.cs
@@ -0,0 +1,18 @@
+﻿public class ArrayStringsAre_Equal
+{
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+    {
+        string wordstr1 = "";
+        foreach (string word in word1)
+        {
+            wordstr1 += word;
+        }
+        string wordstr2 = "";
+        foreach (string word in word2)
+        {
+            wordstr2 += word;
+
+        }
+        return wordstr1.Equals(wordstr2) ? true : false;
+    }
+}
