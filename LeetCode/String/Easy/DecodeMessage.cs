50e7e16d5321e06092940d2b82266112d0371902
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-11 23:44:09 +0530

    11-04-2025 #2

---
+++ LeetCode/String/Easy/DecodeMessage.cs
@@ -0,0 +1,35 @@
+﻿public class Decode_Message
+{
+    public string DecodeMessage(string key, string message)
+    {
+        char ch = 'a';
+        string ans = "";
+        List<Map> Map = new List<Map>();
+        foreach (var item in key)
+        {
+            if (item != ' ' && Map.FirstOrDefault(x => x.key == item) == null)
+            {
+                Map.Add(new Map { key = item, val = ch });
+                ch++;
+            }
+        }
+        foreach (var item in message)
+        {
+            if (item == ' ')
+            {
+                ans += item;
+            }
+            else
+            {
+                ans += Map.FirstOrDefault(x => x.key == item).val;
+            }
+        }
+        return ans;
+    }
+}
+
+public class Map
+{
+    public char key { get; set; }
+    public char val { get; set; }
+}
