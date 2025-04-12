3e4ab1e394191393c77e1fa9079b0a6e52662055
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-12 23:13:03 +0530

    3190. Find Minimum Operations to Make All Elements Divisible by Three

---
+++ LeetCode/Array/Minimum_Operations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    internal class Minimum_Operations
+    {
+        public int MinimumOperations(int[] nums)
+        {
+            int ans = 0;
+            foreach (int n in nums)
+            {
+                if (n % 3 != 0)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
