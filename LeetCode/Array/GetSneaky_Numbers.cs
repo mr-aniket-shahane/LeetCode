6851c3ef1671be26da0786abc8ee2f48b5eef37b
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-12 23:43:47 +0530

    GetSneaky_Numbers

---
+++ LeetCode/Array/GetSneaky_Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    internal class GetSneaky_Numbers
+    {
+        public int[] GetSneakyNumbers(int[] nums)
+        {
+            List<int> ans = new List<int>();
+            List<int> result = new List<int>(nums);
+            foreach (int num in nums)
+            {
+                if (result.Where(a => a.Equals(num)).Count() > 1)
+                {
+                    ans.Add(num);
+                }
+            }
+            return ans.Distinct().ToArray();
+        }
+    }
+}
