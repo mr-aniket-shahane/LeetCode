9d81a4f0c157e3be8f30d0fadb3f2df8531f3692
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-12 22:38:32 +0530

    3467. Transform Array by Parity

---
+++ LeetCode/Array/TransformArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    internal class Transform_Array
+    {
+        public int[] TransformArray(int[] nums)
+        {
+            int odd=0, even=0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+            int[] ans = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(even != 0)
+                {
+                    ans[i] = 0;
+                    even--;
+                }
+                else
+                {
+                    ans[i] = 1;
+                    odd--;
+                }
+            }
+                return ans;
+        }
+    }
+}
