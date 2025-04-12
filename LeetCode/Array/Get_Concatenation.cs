d21784c7764fde26ee42e343218b3a15d0f5ada5
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-12 19:58:52 +0530

    1929. Concatenation of Array

---
+++ LeetCode/Array/Get_Concatenation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    internal class Get_Concatenation
+    { 
+            public int[] GetConcatenation(int[] nums)
+            {
+            return nums;
+            }
+        }
+    }
