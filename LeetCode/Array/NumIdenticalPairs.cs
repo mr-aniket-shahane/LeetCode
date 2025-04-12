ff0c5f8a0c6236ebb4e9d1a4f3f88aed48f74d69
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-12 22:28:02 +0530

    NumIdenticalPairs

---
+++ LeetCode/Array/NumIdenticalPairs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    internal class NumIdentical_Pairs
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int ans = 0;
+            List<int> list = new List<int>(nums);
+            List<int> bklist = new List<int>(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool isAvailable = true;
+                list = bklist.Skip(i + 1).ToList();
+                while (isAvailable)
+                {
+                    var index = list.IndexOf(nums[i]);
+                    if (list.Contains(nums[i]))
+                    {
+                        ans++;
+                        list.RemoveAt(index);
+                    }
+                    else
+                    {
+                        isAvailable = false;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
