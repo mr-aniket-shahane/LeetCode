9ed0252ff76afd2167277f02f3be944ace4a4ac6
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-12 23:22:02 +0530

    Max Wealth

---
+++ LeetCode/Array/Maximum_Wealth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    public class Maximum_Wealth
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int ans = 0;
+            foreach (var account in accounts)
+            {
+                List<int> list = new List<int>(account);
+                var an = list.Sum(a => a);
+                if (ans <= an)
+                {
+                    ans = an;
+                }
+            }
+            return ans;
+        }
+    }
+}
