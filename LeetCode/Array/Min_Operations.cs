88ca24d2def8e7316ad0c9f891e0c7ab78a13d03
Aniket Shahane <aniket.shahane@prosot.com>
2025-04-12 21:42:52 +0530

    1769. Minimum Number of Operations to Move All Balls to Each Box

---
+++ LeetCode/Array/Min_Operations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    internal class Min_Operations
+    {
+        public int[] MinOperations(string boxes)
+        {
+            int len = boxes.Length;
+            int[] ar = new int[len];
+            char[] chars = boxes.ToCharArray();
+
+            int counter = 0;
+            int pointer = 0;
+            for (int j = 0; j < len; j++)
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    if (i != pointer)
+                    {
+                        if (chars[i] == '1')
+                        {
+                            counter = counter + Math.Abs(pointer - i);
+                        }
+                    }
+
+                }
+                ar[pointer] = counter;
+                pointer++;
+                counter = 0;
+            }
+
+            return ar;
+        }
+    }
+}
